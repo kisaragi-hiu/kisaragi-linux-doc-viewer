a87ec67412880a753e214ce105e554b4fb7c36b1
Kisaragi Hiu <mail@kisaragi-hiu.com>
2023-12-05 07:57:03 +0900

    Cleanup

--- src/routes/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-  import DirView from "$lib/DirView.svelte";
-</script>
-
-<DirView path="/usr/share/doc"></DirView>
--- svelte.config.js
+++
@@ -1,9 +0,0 @@
-import adapter from "@sveltejs/adapter-node";
-import { vitePreprocess } from "@sveltejs/kit/vite";
-
-export default {
-  preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter(),
-  },
-};
